22d7ceebdbc0fabadb7eb8c0c5e07609ab34476b
youuz11-cyber <youuz11@gmail.com>
2026-05-18 17:45:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import os
+import time
+import requests
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+def send_telegram_message(text):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+    requests.post(url, data=data)
+
+send_telegram_message("네이버 매물 알림봇 테스트 성공입니다.")
+
 while True:
-    print("네이버 매물 알림 서비스 실행중")
+    print("알림봇 실행중")
+    time.sleep(60)
